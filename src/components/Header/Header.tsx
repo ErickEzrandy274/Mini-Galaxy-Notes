f8f7dee7e0cd40b5dd018d36c840cbe2feaf5b7a
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-19 14:43:06 +0700

    make better UI for navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+	const location = useLocation()
+	const pathname = location.pathname.substring(1)
+
 	return (
 		<nav className="shadow bg-gray-800">
 			<div className="container px-6 py-3 mx-auto md:flex">
 				<div className="flex items-center justify-between">
 					<div>
                         <Link
-                            className="text-2xl font-bold transition-colors duration-200 transform hover:text-white lg:text-3xl text-gray-200"
+                            className="text-2xl font-bold transition-colors duration-200 transform hover:text-white lg:text-3xl text-gray-300"
                             to="/">
-                            Brand
+                            Home
                         </Link>
 					</div>
 
@@ -38,20 +41,16 @@ const Header = () => {
 				<div className="w-full md:flex md:items-center md:justify-between">
 					<div className="flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0">
                         <Link
-                            to="/"
-                            className="px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2">
-                            Home
-                        </Link>
-
-                        <Link
                             to="/list"
-                            className="px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2">
+							className={`px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg 
+								hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2 ${pathname === 'list' && `bg-gray-900`}`}>
                             List Notes
                         </Link>
 
                         <Link
                             to="/create"
-                            className="px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2">
+							className={`px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg 
+								hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2 ${pathname === 'create' && `bg-gray-900`}`}>
                             Create New Note
                         </Link>
 					</div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,63 @@
 import React from "react";
-import MainLayout from "../MainLayout/MainLayout";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
-		<MainLayout>
-			<div>Home</div>
-		</MainLayout>
+		<div className="bg-indigo-900 relative overflow-hidden h-screen cursor-default">
+			<img
+				src="https://www.tailwind-kit.com/images/landscape/6.svg"
+				className="absolute h-full w-full object-cover"
+				alt="cover page"
+			/>
+			<div className="inset-0 bg-black opacity-25 absolute"></div>
+			<header className="absolute top-0 left-0 right-0 z-20">
+				<nav className="container mx-auto px-6 md:px-12 py-4">
+					<div className="md:flex justify-center items-center">
+						<div className="flex justify-between items-center">
+							<div className="md:hidden">
+								<button className="text-white focus:outline-none">
+									<svg
+										className="h-12 w-12"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M4 6H20M4 12H20M4 18H20"
+											stroke="currentColor"
+											strokeWidth="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										></path>
+									</svg>
+								</button>
+							</div>
+						</div>
+						{/* <div className="hidden md:flex items-center">
+							<Link to="/list" className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                                List Notes
+							</Link>
+							<Link to="/create" className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                                Create New Note
+							</Link>
+						</div> */}
+					</div>
+				</nav>
+			</header>
+			<div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+				<div className="w-full flex flex-col items-center relative z-10">
+					<h1 className="font-extrabold text-7xl text-center sm:text-8xl text-white leading-tight mt-4">
+						Welcome to Notes Application
+					</h1>
+					<Link
+						to="/list"
+						className="bg-gray-800 hover:bg-gray-900 py-3 px-4 text-lg text-white font-bold rounded-lg shadow-xl mt-10"
+					>
+						START NOW
+					</Link>
+				</div>
+			</div>
+		</div>
 	);
 };
 
