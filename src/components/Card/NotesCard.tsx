fd5b0e86ab598e7b11b5e4fc96ace4517cbaf8b6
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-19 21:44:19 +0700

    add see button on list card

--- src/components/Card/NotesCard.tsx
+++ src/components/Card/NotesCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import { NotesCardProps } from "./interface";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan, faMagnifyingGlassPlus } from "@fortawesome/free-solid-svg-icons";
 
 const NotesCard: React.FC<NotesCardProps> = ({
 	title,
@@ -14,11 +14,22 @@ const NotesCard: React.FC<NotesCardProps> = ({
 			<div className="card-body">
 				<h2 className="card-title font-bold">{title}</h2>
 				<p className="text-ellipsis overflow-hidden">{body}</p>
-				<p>{archived ? `Archived` : `not Archived`}</p>
-				<p>Created at {createdAt}</p>
+				<p>Status:
+					<span className={`p-1.5 m-1 rounded-lg ${archived ? `bg-base-100` : `bg-base-300`} `}>
+						{archived ? `Archived` : `not Archived`}
+					</span>
+				</p>
+				<p>Created at: {createdAt}</p>
 			</div>
-			
-			<div className="flex justify-end bg-base-100 p-4">
+
+			<div className="flex justify-end bg-base-100 p-4 gap-4">
+				<Button
+					type="button"
+					iconName={faMagnifyingGlassPlus}
+					buttonName="SEE"
+					className="bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 w-24 h-10"
+				/>
+
 				<Button
 					type="button"
 					iconName={faTrashCan}
