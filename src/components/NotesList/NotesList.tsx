7652af5c1eceb548f490c73b7cf7a6f7173404d3
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-20 14:32:00 +0700

    make render conditional either show card notes or not

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NotesCard from "../Card/NotesCard";
 import { DataProps, ListNotesProps } from "../ListPage/interface";
 
@@ -12,8 +13,16 @@ const NotesList: React.FC<DataProps> = ({ data }) => {
 					})}
 				</div>
 			) : (
-				<div>
-					<h2>Tidak ada catatan</h2>
+				<div className="flex flex-col gap-4 text-center items-center my-10 font-semibold text-white">
+						<h2 className="text-5xl md:text-6xl">Tidak ada catatan</h2>
+						<h2 className="text-3xl md:text-4xl">Silahkan buat catatan terlebih dahulu!</h2>
+					<Link
+						to="/create"
+						className={`bg-gray-700 px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg 
+								hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2 w-40 shadow-xl`}
+					>
+						Create New Note
+					</Link>
 				</div>
 			)}
 		</>
