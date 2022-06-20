ed822a7f20ebf80f331fb321845250052de1b486
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-20 15:43:02 +0700

    delete unused tag in home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -9,32 +9,6 @@ const HomePage = () => {
 				alt="cover page"
 			/>
 			<div className="inset-0 bg-black opacity-25 absolute"></div>
-			<header className="absolute top-0 left-0 right-0 z-20">
-				<nav className="container mx-auto px-6 md:px-12 py-4">
-					<div className="md:flex justify-center items-center">
-						<div className="flex justify-between items-center">
-							<div className="md:hidden">
-								<button className="text-white focus:outline-none">
-									<svg
-										className="h-12 w-12"
-										viewBox="0 0 24 24"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M4 6H20M4 12H20M4 18H20"
-											stroke="currentColor"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										></path>
-									</svg>
-								</button>
-							</div>
-						</div>
-					</div>
-				</nav>
-			</header>
 			<div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
 				<div className="w-full flex flex-col items-center relative z-10">
 					<h1 className="font-extrabold text-7xl text-center sm:text-8xl text-white leading-tight mt-4">
