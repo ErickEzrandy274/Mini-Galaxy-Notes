e203c44233efc41cc11583e25398369fdc326437
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-21 15:18:07 +0700

    implement protected route in 3 main feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// reference link private route https://github.com/sairajchouhan/nextjs-firebase-auth/blob/main/pages/_app.tsx
 import { Route, Routes } from "react-router";
 import ArchivedPage from "./components/ArchivedPage/ArchivedPage";
 import Login from "./components/Authentication/Login";
@@ -6,14 +7,36 @@ import CreatePage from "./components/CreatePage/CreatePage";
 import HomePage from "./components/HomePage/HomePage";
 import ListPage from "./components/ListPage/ListPage";
 import NotFound from "./components/NotFound/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
-			<Route path="/list" element={<ListPage />} />
-			<Route path="/create" element={<CreatePage />} />
-			<Route path="/archived" element={<ArchivedPage />} />
+			<Route
+				path="/list"
+				element={
+					<ProtectedRoute>
+						<ListPage />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/create"
+				element={
+					<ProtectedRoute>
+						<CreatePage />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/archived"
+				element={
+					<ProtectedRoute>
+						<ArchivedPage />
+					</ProtectedRoute>
+				}
+			/>
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
 			<Route path="*" element={<NotFound />} />
