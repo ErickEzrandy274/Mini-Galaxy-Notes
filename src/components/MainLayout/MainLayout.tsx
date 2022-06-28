dc6f3104d0f390a56a74707e24db8f4e709f824d
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-28 17:22:20 +0700

    add basic animation

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,13 +1,16 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import Header from "../Header/Header";
 import { MainLayoutProps } from "./interface";
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 	return (
-		<div className="p-2">
-			<Header />
-			{children}
-		</div>
+		<AnimatePresence>
+			<div className="p-2">
+				<Header />
+				{children}
+			</div>
+		</AnimatePresence>
 	);
 };
 
