5c2abc3530f9a30f21fb84f39489ce11eae5c204
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-28 17:45:56 +0700

    create delete confirmation model component

--- src/components/Card/NotesCard.tsx
+++ src/components/Card/NotesCard.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "../Button/IconButton";
 import { faTrashCan, faArchive } from "@fortawesome/free-solid-svg-icons";
 import { ListNotesProps } from "../ListPage/interface";
 import { deleteCard, updateCard } from "../../utils/function/function";
 import { useAuth } from "../../context/AuthContext";
+import ConfirmationModal from "../Modal/ConfirmationModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NotesCard: React.FC<ListNotesProps> = ({
 	title,
@@ -14,6 +16,7 @@ const NotesCard: React.FC<ListNotesProps> = ({
 	lastModified,
 }) => {
 	const { user } = useAuth();
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 	return (
 		<div className="card w-96 bg-primary text-primary-content font-semibold">
@@ -51,20 +54,30 @@ const NotesCard: React.FC<ListNotesProps> = ({
 					}}
 				/>
 
-				<IconButton
-					type="button"
-					iconName={faTrashCan}
-					buttonName="DELETE"
-					className="bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 w-24 h-10"
-					handleClick={() => {
-						if (objKey !== null)
-							deleteCard({
-								uid: user.uid,
-								type: "update",
-								objKey,
-							});
-					}}
-				/>
+				<label
+					htmlFor="confirmationModal"
+					className="flex justify-center items-center gap-2 upppercase bg-red-600 
+						hover:bg-red-700 focus:ring-red-500 focus:ring-2 focus:ring-offset-2 
+						focus:ring-offset-red-200 w-24 h-10 cursor-pointer rounded-lg py-2 px-4"
+					onClick={() => setIsModalOpen(true)}
+				>
+					<FontAwesomeIcon icon={faTrashCan} />
+					Delete
+				</label>
+
+				{isModalOpen && (
+					<ConfirmationModal
+						setModalOpen={setIsModalOpen}
+						onClick={() => {
+							if (objKey !== null)
+								deleteCard({
+									uid: user.uid,
+									type: "delete",
+									objKey,
+								});
+						}}
+					/>
+				)}
 			</div>
 		</div>
 	);
