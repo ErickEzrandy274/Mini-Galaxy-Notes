8c8fa428dd78f27667d43ade90909ff4ee0fc787
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-06 11:25:41 +0700

    implement animate on information of archived page

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -3,10 +3,20 @@ import NotesCard from "../Card/NotesCard";
 import { DataProps, ListNotesProps } from "../ListPage/interface";
 import NewLink from "../NewLink/NewLink";
 import { motion } from "framer-motion";
-import { basicAnimate } from "../Authentication/constant";
+import {
+	basicAnimate,
+	secondAnimate,
+	thirdAnimate,
+} from "../Authentication/constant";
 
 const NotesList: React.FC<DataProps> = ({ data, isArchived }) => {
 	const { initial, animate, transition } = basicAnimate;
+	const {
+		initial: secondInit,
+		animate: secondAnim,
+		transition: secondTrans,
+	} = secondAnimate;
+	const { initial: thirdInit } = thirdAnimate;
 
 	return (
 		<>
@@ -30,12 +40,26 @@ const NotesList: React.FC<DataProps> = ({ data, isArchived }) => {
 					transition={transition}
 					className="flex flex-col gap-4 text-center items-center my-10 font-semibold text-white cursor-default"
 				>
-					<h2 className="text-5xl md:text-6xl">{`Tidak ada catatan${
+					<motion.h2
+						initial={secondInit}
+						animate={secondAnim}
+						exit={initial}
+						transition={secondTrans}
+						className="text-5xl md:text-6xl"
+					>{`Tidak ada catatan${
 						isArchived ? ` yang diarsip` : ``
-					}!`}</h2>
-					<h2 className="text-3xl md:text-4xl">
+					}!`}</motion.h2>
+
+					<motion.h2
+						initial={thirdInit}
+						animate={secondAnim}
+						exit={initial}
+						transition={secondTrans}
+						className="text-3xl md:text-4xl"
+					>
 						Silahkan buat catatan terlebih dahulu!
-					</h2>
+					</motion.h2>
+
 					<NewLink
 						to="/create"
 						linkName="Create New Note"
