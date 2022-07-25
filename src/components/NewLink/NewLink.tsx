b2efe6428e9a9d33cc257a50f4b88e175ce2217e
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-25 14:20:09 +0700

    fix styling on navbar

--- src/components/NewLink/NewLink.tsx
+++ src/components/NewLink/NewLink.tsx
@@ -16,7 +16,7 @@ const NewLink: React.FC<NewLinkProps> = ({
 			? customClass
 			: `px-2 py-1 hover:font-semibold text-lg transition-colors duration-200 transform rounded-lg 
             hover:bg-gray-900 text-gray-200 hover:text-gray-100 md:mx-2 ${
-				pathname === to.substring(1) && `bg-gray-900`
+				pathname === to.substring(1) && `bg-gray-900 font-semibold`
 			}`
 		: `bg-gray-800 hover:bg-gray-900 py-3 px-4 text-lg text-white font-bold rounded-lg shadow-xl mt-10`;
 
