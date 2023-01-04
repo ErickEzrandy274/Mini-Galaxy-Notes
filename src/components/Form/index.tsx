b872e781357da954ae040094ffd96c971e9bd4dd
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-04 09:14:11 +0700

    export all form files

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,5 @@
+export { default as AuthForm } from "./AuthForm";
+export { default as NotesForm } from "./NotesForm";
+export { default as InputAuthForm } from "./InputAuthForm";
+export { default as InputForm } from "./InputForm";
+export * from "./interface";
