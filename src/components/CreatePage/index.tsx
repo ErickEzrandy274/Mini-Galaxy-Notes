2c3ebd13cef5694645835b22fd966584046b2bf3
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-24 12:24:13 +0700

    export all files in create page directory

--- src/components/CreatePage/index.tsx
+++ src/components/CreatePage/index.tsx
@@ -1,14 +1,2 @@
-import { useDocumentTitle } from "utils";
-import { MainLayout, NotesForm } from "components";
-
-const CreatePage = () => {
-	useDocumentTitle("Mini Galaxy Notes App | Create New Notes");
-
-	return (
-		<MainLayout>
-			<NotesForm />
-		</MainLayout>
-	);
-};
-
-export default CreatePage;
+export { default as CreatePage } from "./CreatePage";
+export * from "./constant";
