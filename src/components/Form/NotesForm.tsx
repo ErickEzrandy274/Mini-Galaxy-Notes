bc6f3981b90c97f74cf037849abf37c12092ee59
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-24 12:25:07 +0700

    refactor notes page component

--- src/components/Form/NotesForm.tsx
+++ src/components/Form/NotesForm.tsx
@@ -1,10 +1,8 @@
 import { useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { push } from "firebase/database";
-import { v4 as uuidv4 } from "uuid";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "context";
-import { notesListRef, useHandleChange } from "utils";
+import { handleSubmit, paramsType, useHandleChange } from "utils";
 import { motion } from "framer-motion";
 import {
 	IconButton,
@@ -13,12 +11,15 @@ import {
 	InputType,
 	inputObj,
 	basicAnimate,
+	newNotesInput,
+	newNotesInputType,
 } from "components";
 import toast from "react-hot-toast";
 
 const NotesForm = () => {
 	const { pathname } = useLocation();
 	const { initial, animate, transition } = useMemo(() => basicAnimate, []);
+	const memoizeNewNotesInput = useMemo(() => newNotesInput, []);
 	const {
 		user: { uid },
 	} = useAuth();
@@ -26,36 +27,12 @@ const NotesForm = () => {
 	const { handleChange } = useHandleChange();
 	const navigate = useNavigate();
 
-	const handleSubmit = async (e: any, title: string, body: string) => {
-		e.preventDefault();
-		try {
-			if (title.length < 51) {
-				const newDate = new Date().toISOString();
-
-				const newNotes = {
-					id: uuidv4(),
-					title,
-					body,
-					archived: false,
-					createdAt: newDate,
-					lastModified: newDate,
-				};
-
-				await push(notesListRef({ uid, type: "create", objKey: "" }), newNotes);
-
-				navigate("/list");
-			} else {
-				toast(`Your new notes title is more than 50 character!`);
-			}
-
-			setField(inputObj);
-		} catch (error) {
-			console.error(error);
-		}
+	const createNewNotes = async (params: paramsType) => {
+		await handleSubmit(params);
 	};
 
 	return (
-		<motion.div
+		<motion.section
 			initial={initial}
 			animate={animate}
 			exit={initial}
@@ -63,28 +40,38 @@ const NotesForm = () => {
 			className="flex flex-col sm:w-full w-10/12 max-w-md px-4 mx-auto my-10 lg:my-6 py-5 xl:py-10 rounded-xl
 			shadow bg-gray-800 sm:px-6 md:px-8 lg:px-10 text-white font-semibold"
 		>
-			<div className="self-center mb-6 text-xl  sm:text-4xl text-white font-bold">
+			<h2 className="self-center mb-6 text-xl  sm:text-4xl text-white font-bold">
 				Create New Notes
-			</div>
+			</h2>
 
 			<form
 				className="flex flex-col gap-5"
-				onSubmit={(e) => handleSubmit(e, field.title, field.content)}
-			>
-				<InputForm
-					name="title"
-					placeholder="Insert new title notes"
-					type="text"
-					handleChange={(e) => handleChange(e, setField)}
-					value={field.title}
-				/>
+				onSubmit={(e) => {
+					const { title, content: body } = field;
 
-				<InputForm
-					name="content"
-					placeholder="Insert new content notes"
-					handleChange={(e) => handleChange(e, setField)}
-					value={field.content}
-				/>
+					createNewNotes({
+						e,
+						title,
+						body,
+						toast,
+						uid,
+						navigate,
+						setField,
+					});
+				}}
+			>
+				{memoizeNewNotesInput.map(({ name, type }: newNotesInputType) => {
+					return (
+						<InputForm
+							key={name}
+							name={name}
+							type={type}
+							placeholder={`Insert new ${name} notes`}
+							handleChange={(e) => handleChange(e, setField)}
+							value={field[name]}
+						/>
+					);
+				})}
 
 				<IconButton
 					type="submit"
@@ -96,7 +83,7 @@ const NotesForm = () => {
 
 				{pathname === "/create" && <InfoTooltip {...field} />}
 			</form>
-		</motion.div>
+		</motion.section>
 	);
 };
 
