53be811436df3d8387ec97435f0ded7f2fcfe869
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-04-09 14:39:33 +0700

    refactor: fix context's import path

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context";
+import { AuthContextProvider } from "utils";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
